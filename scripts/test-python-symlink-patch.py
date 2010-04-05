f3f70c202d921523f9a2ca9be2f1e3cc6d2a7aa6
jaraco <none@none>
2010-04-05 13:16:26 +0000

    Moved more of the code into jaraco.develop package

--- scripts/test-python-symlink-patch.py
+++ scripts/test-python-symlink-patch.py
@@ -37,7 +37,7 @@ import urlparse
 from optparse import OptionParser
 from BeautifulSoup import BeautifulSoup
 
-from jaraco.develop.vstudio import find_visual_studio
+from jaraco.develop.vstudio import get_vcvars_env
 
 def init():
 	global test_dir
@@ -102,65 +102,6 @@ def apply_patch():
 	if proc.returncode != 0:
 		print("Error applying patch", file=sys.stderr)
 		raise SystemExit(1)
-
-def validate_pair(ob):
-	try:
-		if not (len(ob) == 2):
-			print("Unexpected result:", ob, file=sys.stderr)
-			raise ValueError
-	except:
-		return False
-	return True
-
-def consume(iter):
-	try:
-		while True: next(iter)
-	except StopIteration:
-		pass
-
-def get_environment_from_batch_command(env_cmd, initial=None):
-	"""
-	Take a command (either a single command or list of arguments)
-	and return the environment created after running that command.
-	Note that if the command must be a batch file or .cmd file, or the
-	changes to the environment will not be captured.
-	
-	If initial is supplied, it is used as the initial environment passed
-	to the child process.
-	"""
-	if not isinstance(env_cmd, (list, tuple)):
-		env_cmd = [env_cmd]
-	# construct the command that will alter the environment
-	env_cmd = subprocess.list2cmdline(env_cmd)
-	# create a tag so we can tell in the output when the proc is done
-	tag = 'Done running command'
-	# construct a cmd.exe command to do accomplish this
-	cmd = 'cmd.exe /s /c "{env_cmd} && echo "{tag}" && set"'.format(**vars())
-	# launch the process
-	proc = subprocess.Popen(cmd, stdout=subprocess.PIPE, env=initial)
-	# parse the output sent to stdout
-	lines = proc.stdout
-	# consume whatever output occurs until the tag is reached
-	consume(itertools.takewhile(lambda l: tag not in l, lines))
-	# define a way to handle each KEY=VALUE line
-	handle_line = lambda l: l.rstrip().split('=',1)
-	# parse key/values into pairs
-	pairs = map(handle_line, lines)
-	# make sure the pairs are valid
-	valid_pairs = filter(validate_pair, pairs)
-	# construct a dictionary of the pairs
-	result = dict(valid_pairs)
-	# let the process finish
-	proc.communicate()
-	return result
-
-def get_vcvars_env(*params):
-	visual_studio = find_visual_studio('9.0')
-	vcvarsall = os.path.join(visual_studio, 'VC', 'vcvarsall.bat')
-	if not os.path.isfile(vcvarsall):
-		print("Couldn't find vcvarsall", file=sys.stderr)
-		raise SystemExit(1)
-	return get_environment_from_batch_command([vcvarsall]+list(params))
 
 def construct_build_command(args=[]):
 	"""
