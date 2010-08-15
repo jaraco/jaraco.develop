a6eeaac55645b7d14be04e6715668dcd4e7f9240
Jason R. Coombs <jaraco@jaraco.com>
2010-08-15 12:55:14 -0400

    Added support for patching the link options as well

--- jaraco/develop/msvc.py
+++ jaraco/develop/msvc.py
@@ -1,3 +1,37 @@
+import functools
+
+def _add_MANIFEST_postarg(link_method):
+	func = link_method.im_func
+	@functools.wraps(func)
+	def wrapper(
+		self,
+		target_desc,
+		objects,
+		output_filename,
+		output_dir=None,
+		libraries=None,
+		library_dirs=None,
+		runtime_library_dirs=None,
+		export_symbols=None,
+		debug=0,
+		extra_preargs=None,
+		extra_postargs=None,
+		build_temp=None,
+		target_lang=None):
+			# make sure extra_postargs isn't None
+			if extra_postargs is None:
+				extra_postargs = []
+			# make sure extra_postargs is a list (so we can append)
+			extra_postargs = list(extra_postargs)
+			# if /MANIFEST isn't present, add it
+			if not '/MANIFEST' in extra_postargs:
+				extra_postargs.append('/MANIFEST')
+			return func(self, target_desc, objects, output_filename,
+				output_dir, libraries, library_dirs, runtime_library_dirs,
+				export_symbols, debug, extra_preargs, extra_postargs,
+				build_temp, target_lang)
+	return wrapper
+
 def patch_msvc9compiler_module():
 	"""
 	msvc9compiler won't use MSVC 10, even though it appears to be compatible.
@@ -16,3 +50,6 @@ def patch_msvc9compiler_module():
 		# We don't have 10.0 either, so the default behavior is fine
 		return
 	mc.VERSION = 10.0
+	
+	# patch MSVCCompiler.link so the /MANIFEST parameter is present
+	mc.MSVCCompiler.link = _add_MANIFEST_postarg(mc.MSVCCompiler.link)
