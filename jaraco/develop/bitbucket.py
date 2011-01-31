d14bb72ac9676ee04b4c57b3fb776e8b7cc78bd1
Jason R. Coombs <jaraco@jaraco.com>
2011-01-30 19:13:35 -0500

    Some attempts to get the REST API to work - no such luck

--- jaraco/develop/bitbucket.py
+++ jaraco/develop/bitbucket.py
@@ -1,18 +1,27 @@
+from __future__ import print_function
+import sys
 import restclient
 import urlparse
 import functools
+import argparse
 
-url = functools.partial(urlparse.urljoin, 'https://api.bitbucket.org/1.0/')
-
-def create_repository(name, auth):
-	print restclient.POST(url('repositories/'), params=dict(name=name),
-		async=False, headers=dict(Authorization=auth))
+def create_repository(name, auth, url):
+	make_url = functools.partial(urlparse.urljoin, url)
+	res = restclient.POST(make_url('repositories/'), params=dict(name=name),
+		async=False, headers=dict(Authorization=auth), accept=['text/json'])
+	if res: print(res, file=sys.stderr)
 
 def create_repository_cmd():
-	import sys
 	from getpass import getuser, getpass
-	auth = ':'.join((getuser(), getpass())).encode('base64')
-	create_repository(sys.argv[1], auth)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('repo_name')
+	parser.add_argument('-a', '--auth')
+	parser.add_argument('-u', '--url', default='https://api.bitbucket.org/1.0/')
+	args = parser.parse_args()
+	if not args.auth:
+		args.auth = ':'.join((getuser(), getpass()))
+	args.auth = args.auth.encode('base64')
+	create_repository(args.repo_name, args.auth, args.url)
 
 if __name__ == '__main__':
 	create_repository_cmd()
