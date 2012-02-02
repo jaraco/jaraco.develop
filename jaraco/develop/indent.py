6878f7fdf8941bff3a29cf701e58d7abbb150e1d
Jason R. Coombs <jaraco@jaraco.com>
2012-02-02 18:28:59 -0500

    Added a command-line parameter to clear the tabs mode.

--- jaraco/develop/indent.py
+++ jaraco/develop/indent.py
@@ -25,13 +25,25 @@ def guess_newline(f):
 	return '\n'
 
 def recursive_glob(spec):
-    """
-    Take a single spec and use the first part as the root and the latter
-    part as the spec.
-    """
-    root, spec = os.path.split(spec)
-    root = root or '.'
-    return jaraco.filesystem.recursive_glob(root, spec)
+	"""
+	Take a single spec and use the first part as the root and the latter
+	part as the spec.
+	"""
+	root, spec = os.path.split(spec)
+	root = root or '.'
+	return jaraco.filesystem.recursive_glob(root, spec)
+
+def remove_tabs_mode(file):
+	with open(file, 'rb') as f:
+		lines = list(f)
+	if not lines:
+		return
+	first_line = lines[0]
+	if (first_line.startswith('#') and 'tab-width' in first_line
+		and 'indent-tabs-mode' in first_line):
+		lines = lines[1:]
+	with open(file, 'wb') as f:
+		f.writelines(lines)
 
 def set_tabs_mode_cmd():
 	"""
@@ -45,6 +57,8 @@ def set_tabs_mode_cmd():
 		action='store_const', const=add_tabs_mode)
 	parser.add_argument('-r', '--recursive', default=glob.glob, dest='glob',
 		action='store_const', const=recursive_glob)
+	parser.add_argument('-c', '--clear', action="store_const",
+		const=remove_tabs_mode, dest='tabs_mode')
 	parser.add_argument('spec', help="The file spec to change")
 	args = parser.parse_args()
 	file_names = args.glob(args.spec)
