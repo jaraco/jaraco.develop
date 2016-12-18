8d1ab208ddfe53a694e39cd718db6bff64d3ab7e
Jason R. Coombs <jaraco@jaraco.com>
2016-12-17 23:26:54 -0500

    Add module for manipulating stuff in pypi (old), including routine for deleting all project documentation.

---
+++ jaraco/develop/pypi.py
@@ -0,0 +1,49 @@
+import re
+import getpass
+import xmlrpc.client
+
+import keyring
+import requests
+
+
+url = 'https://pypi.python.org/pypi'
+client = xmlrpc.client.ServerProxy(url)
+
+def get_projects():
+	username = getpass.getuser()
+	return client.user_packages(username)
+
+
+def request(data):
+	username = getpass.getuser()
+	password = keyring.get_password(url, username)
+	auth = username, password
+	return requests.post(url, data=data, auth=auth)
+
+
+def get_CSRF_token(project):
+	data = {
+		':action': 'pkg_edit',
+		'name': project,
+	}
+	resp = request(data)
+	resp.raise_for_status()
+	res = re.search('"CSRFToken" value="(.*)"', resp.text)
+	return res.group(1)
+
+
+def remove_docs(project):
+	data = {
+		':action': 'doc_destroy',
+		'name': project,
+		'submit': 'Destroy Documentation',
+		'CSRFToken': get_CSRF_token(project),
+	}
+	resp = request(data)
+	resp.raise_for_status()
+
+
+def remove_all_docs():
+	for role, project in get_projects():
+		print("Removing docs for", project)
+		remove_docs(project)
