4440c3924f172dd7514e974cc589baedfefb11a5
Jason R. Coombs <jaraco@jaraco.com>
2020-10-10 13:58:47 -0400

    ⚫ Fade to black.

--- jaraco/develop/add-github-secret.py
+++ jaraco/develop/add-github-secret.py
@@ -4,5 +4,5 @@ from . import github
 
 
 @autocommand.autocommand(__name__)
-def run(name, value, project : github.Repo = github.Repo.detect()):
+def run(name, value, project: github.Repo = github.Repo.detect()):
     project.add_secret(name, value)
