b35bc2383b51f66f31a66cc01fb11818568f102e
Jason R. Coombs <jaraco@jaraco.com>
2020-10-10 11:31:08 -0400

    Extract project metadata into repo module.

--- jaraco/develop/create-github-release.py
+++ jaraco/develop/create-github-release.py
@@ -1,24 +1,15 @@
-import sys
-import subprocess
-import urllib.parse
-
 import autocommand
 
 from . import github
+from . import repo
 
 
 @autocommand.autocommand(__name__)
 def run():
     session = github.get_session()
-    url, version = subprocess.run(
-        [sys.executable, 'setup.py', '--url', '--version'],
-        check=True,
-        stdout=subprocess.PIPE,
-        text=True,
-    ).stdout.split()
-    tag = 'v' + version
-    project = urllib.parse.urlparse(url).path.strip('/')
-    releases = f'{project}/releases'
+    md = repo.get_project_metadata()
+    tag = 'v' + md.version
+    releases = f'{md.project}/releases'
     resp = session.post(releases, json=dict(tag_name=tag, name=tag))
     resp.ok or print(resp.text)
     resp.raise_for_status()
---
+++ jaraco/develop/repo.py
@@ -0,0 +1,16 @@
+import sys
+import subprocess
+import urllib.parse
+
+import bunch
+
+
+def get_project_metadata():
+    url, version = subprocess.run(
+        [sys.executable, 'setup.py', '--url', '--version'],
+        check=True,
+        stdout=subprocess.PIPE,
+        text=True,
+    ).stdout.split()
+    project = urllib.parse.urlparse(url).path.strip('/')
+    return bunch.Bunch(locals())
