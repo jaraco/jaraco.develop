911e67934d9d6dbe52fc5df1e84e52808f74a284
Jason R. Coombs <jaraco@jaraco.com>
2022-08-25 21:55:39 -0400

    Capture errors in calls. Commit if merging.

--- jaraco/develop/update-projects.py
+++ jaraco/develop/update-projects.py
@@ -17,17 +17,21 @@ def checkout(project):
     with path.TempDir() as dir:
         repo = dir / 'repo'
         repo.mkdir()
-        subprocess.Popen(['git', 'clone', url, repo]).wait()
+        subprocess.check_call(['git', 'clone', url, repo])
         with repo:
             yield
 
 
 def update_project(name):
     resolved = resolve(name)
+    print('\nupdating', name)
     with checkout(resolved):
-        subprocess.Popen(['git', 'pull', 'gh://jaraco/skeleton']).wait()
-        subprocess.Popen(['git', 'mergetool']).wait()
-        subprocess.Popen(['git', 'push']).wait()
+        proc = subprocess.Popen(['git', 'pull', 'gh://jaraco/skeleton'])
+        code = proc.wait()
+        if code:
+            subprocess.check_call(['git', 'mergetool'])
+            subprocess.check_call(['git', 'commit', '--no-edit'])
+        subprocess.check_call(['git', 'push'])
 
 
 @autocommand.autocommand(__name__)
