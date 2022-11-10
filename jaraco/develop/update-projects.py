6eb12f12db7601a58ad0f81cacc75e3cf69ee768
Jason R. Coombs <jaraco@jaraco.com>
2022-11-09 22:16:08 -0500

    Use 'known-merge' for resolving conflicts when updating projects.

--- jaraco/develop/update-projects.py
+++ jaraco/develop/update-projects.py
@@ -34,7 +34,7 @@ def update_project(name):
         proc = subprocess.Popen(['git', 'pull', 'gh://jaraco/skeleton', '--no-edit'])
         code = proc.wait()
         if code:
-            subprocess.check_call(['git', 'mergetool'])
+            subprocess.check_call(['git', 'mergetool', '-t', 'known-merge'])
             subprocess.check_call(['git', 'commit', '--no-edit'])
         subprocess.check_call(['git', 'push'])
 
