8553b77559266132b2746b4c2c1d2058bd9e1c87
Jason R. Coombs <jaraco@jaraco.com>
2023-04-02 21:23:42 -0400

    Add rtd module with support for enabling pr builds.

---
+++ jaraco/develop/rtd.py
@@ -0,0 +1,20 @@
+import functools
+
+import keyring
+from requests_toolbelt import sessions
+
+
+url = 'https://api.readthedocs.org/'
+
+
+@functools.lru_cache()
+def session():
+    auth = 'Token ' + keyring.get_password(url, 'token')
+    session = sessions.BaseUrlSession(url + 'api/v3/')
+    session.headers = dict(Authorization=auth)
+    return session
+
+
+def enable_pr_build(project):
+    slug = project.replace('.', '').replace('_', '-')
+    session().patch(f'projects/{slug}/', data=dict(external_builds_enabled=True))
