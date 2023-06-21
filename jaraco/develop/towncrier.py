85c0b5eaee6c980c2ef5eeddc9d441dbb884caa8
Jason R. Coombs <jaraco@jaraco.com>
2023-06-20 20:31:09 -0400

    Added towncrier module with 'get_version()' function to determine the next version to use.

---
+++ jaraco/develop/towncrier.py
@@ -0,0 +1,46 @@
+import collections
+import pathlib
+
+
+from jaraco.vcs import repo
+
+
+_release_bumps = collections.defaultdict(
+    feature='minor',
+    bugfix='patch',
+    doc='minor',
+    removal='major',
+    misc='patch',
+)
+"""
+Map the towncrier default fragment types to semver bumps.
+"""
+
+
+def _release_bump(file):
+    """
+    Given a towncrier fragment file, determine the
+    semver release bump.
+    """
+    _, type_, _ = file.name.split('.', 2)
+    return _release_bumps[type_]
+
+
+def release_kind():
+    """
+    Determine which release to make based on the files in the
+    changelog.
+    """
+    path = pathlib.Path('newsfragments')
+    fragments = path.iterdir() if path.exists() else ()
+    bumps = map(_release_bump, fragments)
+    # use min here as 'major' < 'minor' < 'patch'
+    return min(bumps, default='patch')
+
+
+def get_version():
+    """
+    >>> str(get_version())
+    '...'
+    """
+    return repo().get_next_version(release_kind())
