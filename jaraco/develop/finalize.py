96b6ddc2314359e2d8b910c3fe5d218020ae04bd
Jason R. Coombs <jaraco@jaraco.com>
2023-07-30 14:59:04 -0400

    During finalize, create the newsfragments directory if it doesn't exist.
    Workaround for twisted/towncrier#538

--- jaraco/develop/finalize.py
+++ jaraco/develop/finalize.py
@@ -1,3 +1,4 @@
+import pathlib
 import subprocess
 
 import autocommand
@@ -8,6 +9,8 @@ from . import towncrier
 @autocommand.autocommand(__name__)
 def finalize():
     ver = towncrier.semver(towncrier.get_version())
+    # workaround for twisted/towncrier#538
+    pathlib.Path('newsfragments').mkdir(exist_ok=True)
     towncrier.run('build', '--yes')
     subprocess.check_call(['git', 'commit', '-a', '-m' 'Finalize'])
     subprocess.check_call(['git', 'tag', '-a', '-m', '', ver])
