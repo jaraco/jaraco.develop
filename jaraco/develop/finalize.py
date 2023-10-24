da30921cb46cfe86962071b921f350091b527c6b
Jason R. Coombs <jaraco@jaraco.com>
2023-10-24 12:08:04 -0400

    Rely on latest towncrier for simple usage without workaround.

--- jaraco/develop/finalize.py
+++ jaraco/develop/finalize.py
@@ -1,4 +1,3 @@
-import pathlib
 import subprocess
 
 import autocommand
@@ -9,8 +8,6 @@ from . import towncrier
 @autocommand.autocommand(__name__)
 def finalize():
     ver = towncrier.semver(towncrier.get_version())
-    # workaround for twisted/towncrier#538
-    pathlib.Path('newsfragments').mkdir(exist_ok=True)
     towncrier.run('build', '--yes')
     subprocess.check_call(['git', 'commit', '-a', '-m' 'Finalize'])
     subprocess.check_call(['git', 'tag', '-a', '-m', '', ver])
