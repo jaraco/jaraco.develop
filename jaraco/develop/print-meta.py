405317aff29aeb43dad2bf61f63b3edd060d841f
Jason R. Coombs <jaraco@jaraco.com>
2024-08-19 18:47:50 -0400

    🧎‍♀️ Genuflect to the types.

--- jaraco/develop/print-meta.py
+++ jaraco/develop/print-meta.py
@@ -1,8 +1,10 @@
+from typing import cast, Any
+
 from build.util import project_wheel_metadata
 from jaraco.ui.main import main
 
 
 @main
 def run(path: str = '.', field: str = 'Requires-Dist'):
-    for spec in project_wheel_metadata(path).get_all(field):
+    for spec in cast(list[Any], project_wheel_metadata(path).get_all(field)):
         print(spec)
