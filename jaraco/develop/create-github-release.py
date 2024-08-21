27cbe817b24966d112cff1bea2f29bb7af6ffdbd
Jason R. Coombs <jaraco@jaraco.com>
2024-08-21 12:04:28 -0400

    Fix issue in create-github-release with typer usage.
    Fixes #23

--- jaraco/develop/create-github-release.py
+++ jaraco/develop/create-github-release.py
@@ -1,9 +1,15 @@
+import typer
 from jaraco.ui.main import main
+from typing_extensions import Annotated
 
 from . import github, repo
 
 
 @main
-def run(project: github.Repo = github.Repo.detect()):
+def run(
+    project: Annotated[
+        github.Repo, typer.Option(parser=github.Repo)
+    ] = github.Repo.detect(),
+):
     md = repo.get_project_metadata()
     project.create_release(tag=f'v{md.version}')
